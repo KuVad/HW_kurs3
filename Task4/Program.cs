3437631bfb861aec3ff279ac9943968d8866a998
kuvad <brosockobry@gmail.com>
2023-06-15 21:38:25 +0500

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа: ");
+int x1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+int x3 = int.Parse(Console.ReadLine());
+int Maximum = x1;
+
+if (x2 > Maximum)   Maximum = x2;
+if (x3 > Maximum)   Maximum = x3;
+
+Console.WriteLine($"Максимальное число: {Maximum}");
